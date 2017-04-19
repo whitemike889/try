bda2092fcfe3eb057c7f4c49fbdacadf4bdc4014
Jon Sequeira <jonsequeira@gmail.com>
2017-04-19 12:46:05 -0700

    add support for code fragments

--- LanguageServer/CompileAndExecuteRequest.cs
+++ LanguageServer/CompileAndExecuteRequest.cs
@@ -2,15 +2,37 @@
 {
     public class CompileAndExecuteRequest
     {
-        private readonly string source;
-
         public CompileAndExecuteRequest(string source)
         {
-            this.source = source ?? "";
+            source = source ?? "";
+
+            if (!source.IsFragment())
+            {
+                Sources = new[] { source };
+            }
+            else
+            {
+                Sources = new[]
+                {
+                    main,
+                    $"{usingStatements} public static class Fragment {{ public static void Invoke() {{ {source} }} }}"
+                };
+            }
         }
 
         public string Language => "csharp";
 
-        public string[] Sources => new[] { source };
+        public string[] Sources { get; }
+
+        private static readonly string usingStatements = @"
+using System; using System.Collections.Generic; using System.Linq;";
+
+        private static readonly string main = usingStatements + @"
+public class Program 
+{
+    public static void Main() {  
+        Fragment.Invoke();
+    }  
+} ";
     }
 }
