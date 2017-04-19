43fc959d3789dd0fdbcca3c4ce69284e75bf19e4
jonsequitur <jonsequeira@gmail.com>
2017-04-19 11:24:42 -0700

    ability to load code from a uri

--- MLS.Agent/App.jsx
+++ MLS.Agent/App.jsx
@@ -5,66 +5,94 @@ import styles from "./style.css";
 class App extends Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
-        code: "// loading...",
-        output: "Click run to get output"
+      code: "// loading...",
+      output: "Click run to get output"
     };
-
+    
     var code;
-
-    if (props.codeUrl) {
-      this.loadCode(props.codeUrl)
-          .then(r => this.setState({
-            code: r
-          }));
-    } else { 
-      code = setState({
-        code: [
-          ` // ${JSON.stringify(props)} `,
-          "using System;",
-          "using System.Collections.Generic;",
-          "",
-          "public class Program",
-          "{", 
-          "    public static void Main()",
-          "    {",
-          "        var names = new List<string>",
-          "        {",
-          "            \"Alice\",",
-          "            \"Bobby\",",
-          "            \"Carol\",",
-          "            \"Diane\"",
-          "        };",
-          "",
-          "        names.ForEach(name => Console.WriteLine(name));",  
-          "    }",
-          "}",
-        ].join('\n')
-        });
+    
+    if (!this.tryLoadCodeFromUrl(props)) {
+      this.loadDefault();
     }
-
+    
     this.onClick = this.onClick.bind(this);
     this.onChange = this.onChange.bind(this);
+  }
+  
+  tryLoadCodeFromUrl (props) {
+    if (!props.location || !props.location.search) {
+      return false;
+    }
+
+    var from = props
+          .location
+          .search
+          .slice(1)
+          .split('&')
+          .map(kv => kv.split('='))
+          .reduce((hash, pair) => { 
+            hash[pair[0]] = pair[1];
+            return hash;
+          }, {})['from'];
+
+      if (!from) {
+        return false;
+      }
+
+      this.state = {
+        code: `// loading from ${decodeURIComponent(from)} ...`,
+        output: "Click run to get output"
+      };
+
+      this.loadCode(from)
+          .then(r => {
+            setTimeout(() => {
+              this.setState({
+                code: r, 
+                loaded: true
+              });
+            }, 3000);
+      });
+      
+      return true;
+  }
+
+  loadDefault() {
+    setTimeout(() => {
+         this.setState({
+            code: 
+`using System;
+
+public class Program  
+{ 
+    public static void Main()
+    {
+        Console.WriteLine(\"Hello!\");  
+    }
+}`,
+          });
+      }, 500);
   }
 
   editorDidMount(editor, monaco) {
     editor.focus();
   }
-
+  
   onChange(newValue, e) {
     console.log(e);
     this.setState({code: newValue});
   }
-
+  
   onClick(e) {
     console.log(e);
     this.compileAndExecute(this.state.code)
-        .then(result => {
-          this.setState({output: result.output.map(l => <span>{l} <br /></span>)});
-        });
+    .then(result => {
+      this.setState({output: result.output.map(l => <span>{l} <br /></span>)});
+    });
   }
-
+  
   compileAndExecute(code)
   {
     return fetch('/api/compile', {
@@ -76,48 +104,48 @@ class App extends Component {
         Source: code 
       })
     })
-          .then(r => r.json());
+    .then(r => r.json());
   }
-
+  
   loadCode(uri) {
     return fetch(`/api/code?from=${uri}`, { method: 'GET' })
-           .then(r => r.text());
+    .then(r => r.text());
   }
-
+  
   render() {
-
+    
     const code = this.state.code;
     const options = {
       selectOnLineNumbers: true
     }; 
-
+    
     return (
-      
-      <div className={styles.parent}>
-
-        <div className={styles.editor}>
-          <MonacoEditor
-            width="650"
-            height="350"
-            language="csharp"
-            value={code}
-            options={options}
-            onChange={this.onChange}
-            editorDidMount={this.editorDidMount}
-          />
-        </div>
-        
-        <div className={styles.controls}>
-            <button onClick={this.onClick}>
-              Run
-            </button>
-        </div>
-        
-        <div className={styles.terminal}>
-          {this.state.output}
-        </div>
-
+    
+    <div className={styles.parent}>
+    
+      <div className={styles.editor}>
+        <MonacoEditor
+        width="650"
+        height="350"
+        language="csharp"
+        value={code}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+        />
       </div>
+      
+      <div className={styles.controls}>
+        <button onClick={this.onClick}>
+          Run
+        </button>
+      </div>
+      
+      <div className={styles.terminal}>
+        {this.state.output}
+      </div>
+    
+    </div>
     );
   }
 }
--- MLS.Agent/index.jsx
+++ MLS.Agent/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import { AppContainer } from 'react-hot-loader'
 import 'react-hot-loader/patch';
 import App from './App.jsx'
 
+
+const Routes = (props) => (
+  <Router {...props}>
+    <Route path="/" component={App} />
+  </Router>
+);
+
 const render = App => {
   ReactDOM.render(
     <AppContainer>
-      <App codeUrl={ 'https%3A%2F%2Fgist.githubusercontent.com%2Fjonsequitur%2Fb36ec7591de8f2fa58b99278953cd557%2Fraw%2F836753209d481333d9cf3d01d9897b1e1cad8eb6%2Fdeferred%252520execution%252520of%252520Select' } />
+      <Routes>
+        <App />
+      </Routes>
     </AppContainer>,
     document.getElementById('root')
   )
