f3c406a04bae3c3385ec2b604ca62bc02dde60f1
jonsequitur <jonsequeira@gmail.com>
2017-05-04 09:22:15 -0700

    ScriptingWorkspaceServer more-or-less parity with CLI-based server

--- WorkspaceServer/ProcessResult.cs
+++ WorkspaceServer/ProcessResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WorkspaceServer
 {
@@ -6,14 +7,29 @@ namespace WorkspaceServer
     {
         public ProcessResult(
             bool succeeded,
-            IReadOnlyCollection<string> output)
+            IReadOnlyCollection<string> output,
+            IReadOnlyCollection<LineInfo> lineInfo = null,
+            object returnValue = null)
         {
+            Output = output ?? throw new ArgumentNullException(nameof(output));
             Succeeded = succeeded;
-            Output = output;
+            LineInfo = lineInfo ?? Array.Empty<LineInfo>();
+            ReturnValue = returnValue;
         }
 
         public bool Succeeded { get; }
 
         public IReadOnlyCollection<string> Output { get; }
+
+        public object ReturnValue { get; }
+
+        public IReadOnlyCollection<LineInfo> LineInfo { get; }
+
+        public override string ToString() =>
+            $"Succeeded: {Succeeded}\n{string.Join("\n", Output)}";
+    }
+
+    public class LineInfo
+    {
     }
 }
---
+++ WorkspaceServer/RedirectConsoleOutput.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+
+namespace WorkspaceServer
+{
+    public class RedirectConsoleOutput : IDisposable
+    {
+        private readonly TextWriter originalWriter;
+        private readonly StringWriter writer = new StringWriter();
+
+        public RedirectConsoleOutput()
+        {
+            originalWriter = Console.Out;
+            Console.SetOut(writer);
+        }
+
+        public void Dispose() => Console.SetOut(originalWriter);
+
+        public override string ToString() => writer.ToString();
+    }
+}
