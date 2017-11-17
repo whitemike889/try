dff424b5efd3a97f5b94f186e58baca74c285f4f
Ty Overby <tyoverby@microsoft.com>
2017-11-17 22:58:58 +0000

    Merged PR 94861: prints the certificate key
    Agent stage one of the encryption-in-transit work.
    
    This code will help me validate that the orchestrator code is correct before turning the encryption on in order to mitigate live-site issues.

--- MLS.Agent/Program.cs
+++ MLS.Agent/Program.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using static Pocket.Logger<MLS.Agent.Program>;
+using Pocket;
 using Microsoft.AspNetCore.Hosting;
 
 namespace MLS.Agent
 {
     public class Program
     {
+        public static X509Certificate2 ParseKey(string base64EncodedKey)
+        {
+            var bytes = Convert.FromBase64String(base64EncodedKey);
+            return new X509Certificate2(bytes);
+        }
+
+        internal static IWebHost host = null;
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
+            if (args.Length == 1)
+            {
+                var cert = ParseKey(args[0]);
+                Log.Info($"CERTIFICATE: {args[0]} - {cert.GetCertHashString()}");
+            }
+            else
+            {
+                Log.Warning("No X509Certificate Provided To Main");
+            }
+
+            host = new WebHostBuilder()
+                .UseKestrel(configure =>
+                {
+                    configure.AddServerHeader = false;
+                })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
