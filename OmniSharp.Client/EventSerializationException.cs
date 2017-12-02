c0ca27be04e6eb4e5b6f8980fa6aadbd48cb362d
jonsequitur <jonsequeira@gmail.com>
2017-12-02 13:56:45 -0800

    basic test for event serialization

---
+++ OmniSharp.Client/EventSerializationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OmniSharp.Client
+{
+    public class EventSerializationException : Exception
+    {
+        public EventSerializationException(string message) : base(message)
+        {
+        }
+    }
+}
--- OmniSharp.Client/Serializer.cs
+++ OmniSharp.Client/Serializer.cs
@@ -9,11 +9,13 @@ namespace OmniSharp.Client
         public static OmnisharpEvent DeserializeEvent(string json)
         {
             var @event = JsonConvert.DeserializeObject<EventEnvelope>(json);
+
             if (@event.Event == "ProjectAdded")
             {
                 return @event.Body.ToObject<ProjectAdded>();
             }
-            return null;
+
+            throw new EventSerializationException($"Unknown event type: {json}");
         }
 
         private class EventEnvelope
