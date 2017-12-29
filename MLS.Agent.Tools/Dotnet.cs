dcdc3eca46f5029dd6d618424bb1aaf7fefad033
Jon Sequeira <jonsequeira@gmail.com>
2017-12-29 15:23:28 -0800

    return CommandLineResult

--- MLS.Agent.Tools/Dotnet.cs
+++ MLS.Agent.Tools/Dotnet.cs
@@ -18,17 +18,20 @@ namespace MLS.Agent.Tools
                                 new DirectoryInfo(Directory.GetCurrentDirectory());
         }
 
-        public void New(string templateName, TimeSpan? timeout = null)
+        public CommandLineResult New(string templateName, TimeSpan? timeout = null)
         {
             if (string.IsNullOrWhiteSpace(templateName))
             {
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(templateName));
             }
 
-            Execute($"new {templateName}", timeout);
+            return Execute($"new {templateName}", timeout);
         }
 
-        public void Restore(TimeSpan? timeout = null) =>
+        public CommandLineResult Build(TimeSpan? timeout = null) =>
+            Execute("build", timeout);
+
+        public CommandLineResult Restore(TimeSpan? timeout = null) =>
             Execute("restore", timeout);
 
         public CommandLineResult Run(TimeSpan? timeout = null) =>
