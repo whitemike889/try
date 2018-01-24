ed90c437665c88b736c568a72fc11bfc4710787a
Jon Sequeira <jonsequeira@gmail.com>
2018-01-24 11:47:19 -0800

    add a test to show that workspaceType != script cannot compile a code fragment

--- MLS.Agent.Tests/ApiViaHttpTests.cs
+++ MLS.Agent.Tests/ApiViaHttpTests.cs
@@ -90,6 +90,32 @@ namespace MLS.Agent.Tests
         }
 
         [Fact]
+        public async Task When_a_non_script_workspace_type_is_specified_then_code_fragments_cannot_be_compiled_successfully()
+        {
+            var registry = new WorkspaceServerRegistry();
+            registry.AddWorkspace("console", o => o.CreateUsingDotnet("console"));
+            disposables.Add(registry);
+
+            var requestJson = JsonConvert.SerializeObject(new
+            {
+                Source = @"Console.WriteLine(""hello!"");",
+                WorkspaceType = "console"
+            });
+
+            var response = await CallRun(requestJson, registry);
+
+            var result = await response
+                               .EnsureSuccess()
+                               .DeserializeAs<RunResult>();
+
+            result.ShouldFailWithOutput(
+                "(1,19): error CS1022: Type or namespace definition, or end-of-file expected",
+                "(1,19): error CS1026: ) expected",
+                "(1,1): error CS5001: Program does not contain a static 'Main' method suitable for an entry point"
+            );
+        }
+
+        [Fact]
         public async Task When_they_load_a_snippet_then_they_get_diagnostics_for_the_first_line()
         {
             var output = Guid.NewGuid().ToString();
