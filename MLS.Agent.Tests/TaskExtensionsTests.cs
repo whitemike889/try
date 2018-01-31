926675bfc0e079fbad90e64b9bfe035afbed403e
Jon Sequeira <jonsequeira@gmail.com>
2018-01-30 17:27:15 -0800

    remove timing dependency

--- MLS.Agent.Tests/TaskExtensionsTests.cs
+++ MLS.Agent.Tests/TaskExtensionsTests.cs
@@ -12,13 +12,14 @@ namespace MLS.Agent.Tests
         [Fact]
         public void CancelAfter_can_use_a_CancellationToken_to_cancel_a_non_cancellable_task()
         {
-            var source = new CancellationTokenSource(1.Seconds());
+            var source = new CancellationTokenSource();
 
             Func<Task> x = async () => await Task.Run(async () =>
             {
                 while (true)
                 {
                     await Task.Delay(100);
+                    source.Cancel();
                 }
             }).CancelAfter(source.Token);
 
@@ -28,13 +29,14 @@ namespace MLS.Agent.Tests
         [Fact]
         public void CancelAfter_T_can_use_a_CancellationToken_to_cancel_a_non_cancellable_task()
         {
-            var source = new CancellationTokenSource(1.Seconds());
+            var source = new CancellationTokenSource();
 
             Func<Task<bool>> x = async () => await Task.Run(async () =>
             {
                 while (true)
                 {
                     await Task.Delay(100);
+                    source.Cancel();
                 }
 
                 return true;
@@ -46,13 +48,14 @@ namespace MLS.Agent.Tests
         [Fact]
         public async Task CancelAfter_can_return_a_fallback_value_rather_than_throw_if_cancellation_occurs()
         {
-            var source = new CancellationTokenSource(1.Seconds());
+            var source = new CancellationTokenSource();
 
             var result = await Task.Run(async () =>
             {
                 while (true)
                 {
                     await Task.Delay(100);
+                    source.Cancel();
                 }
 
                 return "not cancelled";
@@ -65,7 +68,7 @@ namespace MLS.Agent.Tests
         [Fact]
         public async Task CancelAfter_returns_the_value_from_a_task_that_completes_before_cancellation()
         {
-            var source = new CancellationTokenSource(1.Seconds());
+            var source = new CancellationTokenSource(10.Seconds());
 
             var result = await Task.Run(() => true)
                                    .CancelAfter(source.Token);
