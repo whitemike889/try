d0387d701dea6d2ba8254fd31091ba093746c729
Jon Sequeira <jonsequeira@gmail.com>
2018-03-01 14:24:32 -0800

    use Assent to guard against incidental RunResult contract changes

---
+++ WorkspaceServer.Tests/RunResultContractTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Assent;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+using Recipes;
+using WorkspaceServer.Models.Execution;
+using Xunit;
+
+namespace WorkspaceServer.Tests
+{
+    public class ContractTests
+    {
+        private readonly Configuration configuration;
+
+        public ContractTests()
+        {
+            configuration = new Configuration()
+                .UsingExtension("json");
+
+#if DEBUG
+          configuration =   configuration.SetInteractive(false);
+#endif
+        }
+
+        [Fact]
+        public void RunResult()
+        {
+            var runResult = new RunResult(
+                true,
+                new[] { "output one", "output two", "output three" },
+              diagnostics:  new[] { new SerializableDiagnostic(
+                  start: 1,
+                  end: 4,
+                  message: "oops",
+                  severity: DiagnosticSeverity.Error,
+                  id: "CS1234")});
+
+            this.Assent(runResult.ToJson(new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            }), configuration);
+        }
+    }
+}
