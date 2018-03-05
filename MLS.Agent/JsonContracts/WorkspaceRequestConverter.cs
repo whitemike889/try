2b1c4c170ee3b2df4ef5f4c46c60a9e9f77ae96b
Diego <dicolomb@microsoft.com>
2018-03-05 18:12:46 +0000

    fix issue with reference

--- MLS.Agent/JsonContracts/WorkspaceRequestConverter.cs
+++ MLS.Agent/JsonContracts/WorkspaceRequestConverter.cs
@@ -15,50 +15,68 @@ namespace MLS.Agent.JsonContracts
 
         static WorkspaceRequestConverter()
         {
-            WorkspaceSignature = new HashSet<string>(typeof(Workspace).GetConstructors()?.SelectMany(c => c.GetParameters()?.Select(p => p.Name)) ?? Enumerable.Empty<string>(), StringComparer.OrdinalIgnoreCase);
-            WorkspaceEnvelopeSignature = new HashSet<string>(typeof(WorkspaceRequest).GetConstructors()?.SelectMany(c => c?.GetParameters().Select(p => p.Name)) ?? Enumerable.Empty<string>(), StringComparer.OrdinalIgnoreCase);
+            WorkspaceSignature = new HashSet<string>(typeof(Workspace).GetConstructors()?.SelectMany(c => c.GetParameters()?.Select(p => p.Name)) ?? Enumerable.Empty<string>(),
+                StringComparer.OrdinalIgnoreCase);
+            WorkspaceEnvelopeSignature =
+                new HashSet<string>(typeof(WorkspaceRequest).GetConstructors()?.SelectMany(c => c?.GetParameters().Select(p => p.Name)) ?? Enumerable.Empty<string>(),
+                    StringComparer.OrdinalIgnoreCase);
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
+            var converter = serializer.Converters.FirstOrDefault(e => e.GetType() == GetType());
+            RemoveConverter(converter, serializer);
             serializer.Serialize(writer, value);
+            RestoreConverter(serializer, converter);
         }
+
+       
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-          
             var obj = serializer.Deserialize(reader) as JObject;
 
             var isWorkspace = obj?.Properties().All(p => WorkspaceSignature.Contains(p.Name)) == true;
             var isWorkspaceEnvelope = obj?.Properties().All(p => WorkspaceEnvelopeSignature.Contains(p.Name)) == true;
-
+            var converter = serializer.Converters.FirstOrDefault(e => e.GetType() == GetType());
             if (isWorkspace)
             {
+                RemoveConverter(converter, serializer);
                 var ws = obj.ToObject<Workspace>();
-                return new WorkspaceRequest(ws);
+                var ret = new WorkspaceRequest(ws);
+                RestoreConverter(serializer, converter);
+                return ret;
             }
 
             if (isWorkspaceEnvelope)
             {
-                serializer.Converters.Remove(this);
+                RemoveConverter(converter, serializer);
                 var ret = obj.ToObject<WorkspaceRequest>(serializer);
-                serializer.Converters.Add(this);
+                RestoreConverter(serializer, converter);
                 return ret;
             }
 
             return null;
         }
-
         public override bool CanConvert(Type objectType)
         {
             var targeType = typeof(WorkspaceRequest);
-            var response =  targeType == objectType || objectType.IsSubclassOf(typeof(WorkspaceRequest));
+            var response = targeType == objectType || objectType.IsSubclassOf(typeof(WorkspaceRequest));
             if (response)
-            {
                 return true;
-            }
 
             return false;
+        }
+
+        private void RemoveConverter(JsonConverter jsonConverter, JsonSerializer serializer)
+        {
+            if (jsonConverter != null)
+                serializer.Converters.Remove(jsonConverter);
+        }
+        private void RestoreConverter(JsonSerializer serializer, JsonConverter converter)
+        {
+            if (converter != null)
+                serializer.Converters.Add(this);
         }
     }
 }
