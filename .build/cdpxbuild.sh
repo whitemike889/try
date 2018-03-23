c979c06d8b2a2362fcfc4a9fd904167862eb57e4
Piotr Puszkiewicz <piotrp@microsoft.com>
2018-03-23 14:52:27 -0700

    -L, but ofStab!

--- .build/cdpxbuild.sh
+++ .build/cdpxbuild.sh
@@ -8,4 +8,4 @@ dotnet restore ./MLS-LS.sln
 dotnet publish ./MLS-LS.sln -c Release -o ./obj/Docker/publish
 cp ./MLS.Agent/Dockerfile ./MLS.Agent/obj/Docker/publish/Dockerfile
 mkdir -p ./MLS.Agent/obj/Docker/publish/dependencies/omnisharp
-curl https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.29.0-beta1/omnisharp-linux-x64.tar.gz -o ./MLS.Agent/obj/Docker/publish/omnisharp.tar.gz
+curl -L https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.29.0-beta1/omnisharp-linux-x64.tar.gz -o ./MLS.Agent/obj/Docker/publish/omnisharp.tar.gz
