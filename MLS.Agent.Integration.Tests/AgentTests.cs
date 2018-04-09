8176a9aa41b207bf6672974eb530941ac6f3eef3
Diego <dicolomb@microsoft.com>
2018-04-09 20:12:55 +0100

    provide http client time out

--- MLS.Agent.Integration.Tests/AgentTests.cs
+++ MLS.Agent.Integration.Tests/AgentTests.cs
@@ -1,31 +1,46 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Peaky.Client;
 using Peaky.XUnit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MLS.Agent.Integration.Tests
 {
-    public class AgentTests : PeakyXunitTestBase, IDisposable
+    public class AgentTestsDiscovery : PeakyXunitTestBase, IDisposable
     {
-        private readonly PeakyClient _peakyClient = new PeakyClient(new Uri("https://mls-monitoring.azurewebsites.net/tests/staging/agent?deployment=true"));
+        private static readonly Uri TestDiscoveryUri = new Uri("https://mls-monitoring.azurewebsites.net/tests/staging/agent?deployment=true");
+
+        private readonly PeakyClient _peakyClient = new PeakyClient(TestDiscoveryUri);
 
         public override PeakyClient PeakyClient => _peakyClient;
-        
 
+        public void Dispose() => _peakyClient.Dispose();
+    }
+
+    public class AgentTests : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly PeakyClient _peakyClient = new PeakyClient(new HttpClient(){Timeout = TimeSpan.FromMinutes(10)});
+
+        public AgentTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
         [Theory]
-        [ClassData(typeof(AgentTests))]
+        [ClassData(typeof(AgentTestsDiscovery))]
         public async Task The_peaky_test_passes(Uri url)
         {
-            var result = await PeakyClient.GetResultFor(url);
-            
+            var result = await _peakyClient.GetResultFor(url);
+
+            _output.WriteLine(result.Content);
+
             result.Passed.Should().BeTrue();
         }
-        
-        public void Dispose()
-        {
-            _peakyClient.Dispose();
-        }
+
+        public void Dispose() => _peakyClient.Dispose();
     }
 }
--- MLS.LanguageServices.Integration.Tests/LanguageServicesTests.cs
+++ MLS.LanguageServices.Integration.Tests/LanguageServicesTests.cs
@@ -1,31 +1,47 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Peaky.Client;
 using Peaky.XUnit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MLS.LanguageServices.Integration.Tests
 {
-    public class LanguageServicesTests : PeakyXunitTestBase, IDisposable
+
+    public class LanguageServicesTestsDiscovery : PeakyXunitTestBase, IDisposable
     {
-        private readonly PeakyClient _peakyClient = new PeakyClient(new Uri("https://mls-monitoring.azurewebsites.net/tests/staging/LanguageServices?deployment=true"));
+        private static readonly Uri TestDiscoveryUri = new Uri("https://mls-monitoring.azurewebsites.net/tests/staging/LanguageServices?deployment=true");
+
+        private readonly PeakyClient _peakyClient = new PeakyClient(TestDiscoveryUri);
 
         public override PeakyClient PeakyClient => _peakyClient;
 
+        public void Dispose() => _peakyClient.Dispose();
+    }
+    public class LanguageServicesTests :  IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly PeakyClient _peakyClient = new PeakyClient(new HttpClient() { Timeout = TimeSpan.FromMinutes(10) });
+
+        public LanguageServicesTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
         [Theory]
-        [ClassData(typeof(LanguageServicesTests))]
+        [ClassData(typeof(LanguageServicesTestsDiscovery))]
         public async Task The_peaky_test_passes(Uri url)
         {
-            var result = await PeakyClient.GetResultFor(url);
+            var result = await _peakyClient.GetResultFor(url);
+
+            _output.WriteLine(result.Content);
 
             result.Passed.Should().BeTrue();
         }
 
-        public void Dispose()
-        {
-            _peakyClient.Dispose();
-        }
+        public void Dispose() => _peakyClient.Dispose();
     }
 }
