0ebcad93c41f24a8500f2bc69682220063786b48
Ravi Chande <rchande@microsoft.com>
2018-04-10 12:19:56 -0700

    Enable container reuploading

--- .build/cdpxbuild.sh
+++ .build/cdpxbuild.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
+set -x
 PWD=$(pwd)
 PWD=$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH
 echo $PWD
 DOCKER_CONTEXT_ROOT=$PWD/MLS.Agent/obj/Docker/publish
 WORKSPACES_ROOT=$DOCKER_CONTEXT_ROOT/workspaces
 DEPENDENCIES_ROOT=$DOCKER_CONTEXT_ROOT/dependencies
+
+echo $BUILD_SOURCEVERSION > build.artifact.commit.sha
 
 cd /source
 pwd
