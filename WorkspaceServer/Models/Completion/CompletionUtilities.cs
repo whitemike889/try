b2ecf9f78e543958afaf7d2c4753b6273d1ef6e1
Diego <dicolomb@microsoft.com>
2018-04-11 23:47:46 +0100

    filter completions using completion text

--- WorkspaceServer/Models/Completion/CompletionUtilities.cs
+++ WorkspaceServer/Models/Completion/CompletionUtilities.cs
@@ -7,6 +7,32 @@ namespace WorkspaceServer.Models.Completion
 {
     internal static class CompletionUtilities
     {
+        private sealed class CompletionTextKindEqualityComparer : IEqualityComparer<AutoCompleteResponse>
+        {
+            public bool Equals(AutoCompleteResponse x, AutoCompleteResponse y)
+            {
+                if (ReferenceEquals(x, y))
+                    return true;
+                if (ReferenceEquals(x, null))
+                    return false;
+                if (ReferenceEquals(y, null))
+                    return false;
+                if (x.GetType() != y.GetType())
+                    return false;
+                return string.Equals(x.CompletionText, y.CompletionText) && string.Equals(x.Kind, y.Kind);
+            }
+
+            public int GetHashCode(AutoCompleteResponse obj)
+            {
+                unchecked
+                {
+                    return ((obj.CompletionText != null ? obj.CompletionText.GetHashCode() : 0) * 397) ^ (obj.Kind != null ? obj.Kind.GetHashCode() : 0);
+                }
+            }
+        }
+
+        public static IEqualityComparer<AutoCompleteResponse> CompletionTextKindComparer { get; } = new CompletionTextKindEqualityComparer();
+
         public static string GetWordAt(this string source, int position)
         {
             var index = position;
@@ -28,13 +54,6 @@ namespace WorkspaceServer.Models.Completion
         {
             var source = items ?? Array.Empty<AutoCompleteResponse>();
 
-            var transformed = source.Select(item => new CompletionItem(
-                item.DisplayText,
-                item.Kind,
-                item.CompletionText,
-                item.CompletionText,
-                item.CompletionText,
-                item.Description));
             var transformed = source.Distinct(CompletionTextKindComparer).Select(item => new CompletionItem(
                 displayText: item.CompletionText,
                 kind: item.Kind,
