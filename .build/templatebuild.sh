68398525d397e8dc6633e7f1c745449bde9cf113
Piotr Puszkiewicz <piotrp@microsoft.com>
2018-05-11 13:38:48 -0700

    Import armgen and use it to generate arm template for agent staging

---
+++ .build/templatebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+REPO_ROOT=`dirname "$0"`; REPO_ROOT=`eval "cd \"$REPO_ROOT/..\" && pwd"`
+
+cd $REPO_ROOT
+
+ls .
+
+export TAG=$(<"build.artifact.commit.sha")
+export IMAGE="mls.orchestrator"
+printenv
+
+cd $REPO_ROOT/express/template-generator
+npm i
+npm run build
