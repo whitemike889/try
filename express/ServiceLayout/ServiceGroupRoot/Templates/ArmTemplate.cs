cab76ca2e822c9dea8a3551e664e7dc001481b97
Piotr Puszkiewicz <piotrp@microsoft.com>
2018-05-11 14:18:59 -0700

    ungitignore express

---
+++ express/ServiceLayout/ServiceGroupRoot/Templates/ArmTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace express
+{
+    public static class ArmTemplate
+    {
+        static public Dictionary<string, string> AsDictionary()
+        {
+            var assembly = typeof(express.ArmTemplate).Assembly;
+            var stream = assembly.GetManifestResourceStream("express.template.json");
+            using (var reader = new StreamReader(stream))
+            {
+                var armTemplate = JObject.Parse(reader.ReadToEnd());
+
+                return armTemplate.SelectTokens("$.resources[?(@.name=='trydotnet-westus')].resources[?(@.type=='config' && @.name=='connectionstrings')].properties")
+                    .Values<JProperty>()
+                    .ToDictionary(t => t.Name, t => t.Value["value"].Value<string>());
+            }
+        }
+    }
+}
