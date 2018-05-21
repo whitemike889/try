13909d2a42172d49c4d425634137c7d292c96e64
Nelson Wu <t-newu@microsoft.com>
2018-05-21 21:32:58 +0000

    Merged PR 123846: Update OmniSharp.Emit
    This is the OmniSharp.Emit part of the instrumentation feature that was broken off from the other PR.
    
    Tests:
    - All unit tests in OmniSharp.Emit.Tests pass
    
    Related work items: #616144

---
+++ OmniSharp.Emit/Augmentation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+
+namespace OmniSharp.Emit
+{
+    public class Augmentation
+    {
+        public Augmentation(StatementSyntax associatedStatment, IEnumerable<ISymbol> locals, IEnumerable<ISymbol> fields, IEnumerable<ISymbol> parameters)
+        {
+            AssociatedStatement = associatedStatment ?? throw new ArgumentNullException(nameof(associatedStatment));
+            Locals = locals ?? Array.Empty<ISymbol>();
+            Fields = fields ?? Array.Empty<ISymbol>();
+            Parameters = parameters ?? Array.Empty<ISymbol>();
+        }
+
+        public StatementSyntax AssociatedStatement { get; }
+
+        public IEnumerable<ISymbol> Locals { get; }
+
+        public IEnumerable<ISymbol> Fields { get; }
+
+        public IEnumerable<ISymbol> Parameters { get; }
+
+
+    }
+}
