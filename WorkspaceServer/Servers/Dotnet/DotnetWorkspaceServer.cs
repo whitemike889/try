b05f9a9facb6d5072c550f9b2ca30d5812d28c1c
Diego <dicolomb@microsoft.com>
2018-05-22 20:27:34 +0100

    enable full log

--- WorkspaceServer/Servers/Dotnet/DotnetWorkspaceServer.cs
+++ WorkspaceServer/Servers/Dotnet/DotnetWorkspaceServer.cs
@@ -44,7 +44,7 @@ namespace WorkspaceServer.Servers.Dotnet
             _omniSharpServer = new OmniSharpServer(
                 _workspace.Directory,
                 Paths.EmitPlugin,
-                logToPocketLogger: false);
+                logToPocketLogger: true);
 
             _initialized = new AsyncLazy<bool>(async () => await Initialise());
         }
