91471e7502260a9d6dcdbec77b13ac0eb888aebb
Diego <dicolomb@microsoft.com>
2018-05-23 23:15:05 +0100

    disable routes for sensors

--- MLS.Agent/Controllers/SensorsController.cs
+++ MLS.Agent/Controllers/SensorsController.cs
@@ -16,6 +16,7 @@ namespace MLS.Agent.Controllers
         {
             _workspaceServerRegistry = workspaceServerRegistry ?? throw new ArgumentNullException(nameof(workspaceServerRegistry));
         }
+
         [Route("/sensors/version")]
         public IActionResult GetVersion() => Ok(AssemblyVersionSensor.Version());
 
@@ -26,14 +27,14 @@ namespace MLS.Agent.Controllers
             return Ok(info);
         }
 
-        [Route("/sensors/getNugetCache")]
+        //[Route("/sensors/getNugetCache")]
         public IActionResult GetNugetCache()
         {
             var dirs = Directory.EnumerateFileSystemEntries(Paths.NugetCache,"*.dll", SearchOption.AllDirectories ).OrderBy(path => path).ToArray();
             return Ok(dirs);
         }
 
-        [Route("/sensors/environmentVariables")]
+        //[Route("/sensors/environmentVariables")]
         public IActionResult GetEnvironmentVariables()
         {
             return Ok(Environment.GetEnvironmentVariables());
