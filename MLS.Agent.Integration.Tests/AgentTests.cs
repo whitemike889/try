70262e3267ad623e9525c8b43a5a0c273f52cdbd
Diego <dicolomb@microsoft.com>
2018-06-19 21:46:19 +0100

    change retry logic and shorter timeout on http client

--- MLS.Agent.Integration.Tests/AgentTests.cs
+++ MLS.Agent.Integration.Tests/AgentTests.cs
@@ -11,7 +11,8 @@ namespace MLS.Agent.Integration.Tests
     public class AgentTests : IDisposable
     {
         private readonly ITestOutputHelper _output;
-
+        private static readonly TimeSpan HttpClientTimeout = TimeSpan.FromMinutes(1);
+        private const int RetryCount = 10;
         private readonly PeakyClient _peakyClient = new PeakyClient(new HttpClient(){Timeout = TimeSpan.FromMinutes(10)});
 
         public AgentTests(ITestOutputHelper output)
@@ -22,18 +23,41 @@ namespace MLS.Agent.Integration.Tests
         [ClassData(typeof(AgentTestsDiscovery))]
         public async Task The_peaky_test_passes(Uri url)
         {
-             await Task.Delay(10000);
-            var result = await _peakyClient.GetResultFor(url);
-
-            if (!result.Passed)
+            var attempts = 0;
+            TestResult result;
+            do
             {
-                await Task.Delay(60000);
-                result = await _peakyClient.GetResultFor(url);
-            }
+                await Task.Delay(10000 * attempts);
+                try
+                {
+                    result = await _peakyClient.GetResultFor(url);
+                    if (result.Passed)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        _output.WriteLine($"Failed attempt with outcome {result.Content}");
+                        result = null;
+                    }
+                }
+                catch (Exception e)
+                {
+                    _output.WriteLine($"Failed attempt with exception {e}");
+                    result = null;
+                }
+                finally
+                {
+                    attempts++;
+                }
 
-            _output.WriteLine(result.Content);
+            } while (attempts < RetryCount);
 
-            result.Passed.Should().BeTrue();
+            result.Should().NotBeNull("All attempts failed");
+
+            _output.WriteLine(result?.Content);
+
+            result?.Passed.Should().BeTrue();
         }
 
         public void Dispose() => _peakyClient.Dispose();
--- MLS.LanguageServices.Integration.Tests/LanguageServicesTests.cs
+++ MLS.LanguageServices.Integration.Tests/LanguageServicesTests.cs
@@ -11,8 +11,9 @@ namespace MLS.LanguageServices.Integration.Tests
     public class LanguageServicesTests :  IDisposable
     {
         private readonly ITestOutputHelper _output;
-
-        private readonly PeakyClient _peakyClient = new PeakyClient(new HttpClient() { Timeout = TimeSpan.FromMinutes(10) });
+        private static readonly TimeSpan HttpClientTimeout = TimeSpan.FromMinutes(1);
+        private const int RetryCount = 10;
+        private readonly PeakyClient _peakyClient = new PeakyClient(new HttpClient() { Timeout = HttpClientTimeout });
 
         public LanguageServicesTests(ITestOutputHelper output)
         {
@@ -23,18 +24,41 @@ namespace MLS.LanguageServices.Integration.Tests
         [ClassData(typeof(LanguageServicesTestsDiscovery))]
         public async Task The_peaky_test_passes(Uri url)
         {
-             await Task.Delay(10000);
-            var result = await _peakyClient.GetResultFor(url);
-
-            if (!result.Passed)
+            var attempts = 0;
+            TestResult result;
+            do
             {
-                await Task.Delay(60000);
-                result = await _peakyClient.GetResultFor(url);
-            }
+                await Task.Delay(10000 * attempts);
+                try
+                {
+                    result = await _peakyClient.GetResultFor(url);
+                    if (result.Passed)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        _output.WriteLine($"Failed attempt with outcome {result.Content}");
+                        result = null;
+                    }
+                }
+                catch (Exception e)
+                {
+                    _output.WriteLine($"Failed attempt with exception {e}");
+                    result = null;
+                }
+                finally
+                {
+                    attempts++;
+                }
 
-            _output.WriteLine(result.Content);
+            } while (attempts < RetryCount);
 
-            result.Passed.Should().BeTrue();
+            result.Should().NotBeNull("All attempts failed");
+
+            _output.WriteLine(result?.Content);
+            
+            result?.Passed.Should().BeTrue();
         }
 
         public void Dispose() => _peakyClient.Dispose();
