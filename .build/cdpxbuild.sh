50c695bad0c9530346aae73ba9f5e561c419cb62
Piotr Puszkiewicz <piotrp@microsoft.com>
2018-07-11 11:27:52 -0700

    First turn of converting build to cdpx

--- .build/cdpxbuild.sh
+++ .build/cdpxbuild.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 set -e
-set -x
-PWD=$(pwd)
-PWD=$CDP_USER_SOURCE_FOLDER_CONTAINER_PATH
-echo $PWD
-DOCKER_CONTEXT_ROOT=$PWD/MLS.Agent/obj/Docker/publish
-WORKSPACES_ROOT=$DOCKER_CONTEXT_ROOT/workspaces
-DEPENDENCIES_ROOT=$DOCKER_CONTEXT_ROOT/dependencies
-cd /source
 
-echo $BUILD_SOURCEVERSION > build.artifact.commit.sha
+# Get absolute path to source root
+REPO_ROOT=`dirname "$0"`; REPO_ROOT=`eval "cd \"$REPO_ROOT/..\" && pwd"`
+cd $REPO_ROOT
+source $REPO_ROOT/.build/conventions.sh
 
-pwd
-ls .
-dotnet restore ./MLS-LS.sln
-dotnet publish ./MLS-LS.sln -c Release -o ./obj/Docker/publish
-cp ./MLS.Agent/Dockerfile $DOCKER_CONTEXT_ROOT/Dockerfile
+# record commit hash
+echo $BUILD_SOURCEVERSION > $REPO_ROOT/build.artifact.commit.sha
+
+dotnet publish $REPO_ROOT/MLS-LS.sln -c Release -o $REPO_ROOT/obj/Docker/publish
+cp $REPO_ROOT/MLS.Agent/Dockerfile $DOCKER_CONTEXT_ROOT/Dockerfile
 
 export NUGET_PACKAGES=$DOCKER_CONTEXT_ROOT/packages
-
-# prepopulate workspaces
-mkdir -p $WORKSPACES_ROOT/console
-pushd $WORKSPACES_ROOT/console
-dotnet new console
-dotnet add package Newtonsoft.Json
-dotnet build
-popd
-
-mkdir -p $WORKSPACES_ROOT/nodatime.api
-pushd $WORKSPACES_ROOT/nodatime.api
-dotnet new console
-dotnet add package Newtonsoft.Json
-dotnet add package NodaTime
-dotnet add package NodaTime.Testing
-dotnet build
-popd
---
+++ .build/cdpxrestore.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+# Get absolute path to source root
+REPO_ROOT=`dirname "$0"`; REPO_ROOT=`eval "cd \"$REPO_ROOT/..\" && pwd"`
+cd $REPO_ROOT
+source $REPO_ROOT/.build/conventions.sh
+
+# Add packages from APK
+apk update
+apk add xmlstarlet
+apk add shadow
+
+# Add dotnet tools
+dotnet tool install t-rex --version 1.0.0-preview1-004 --add-source https://www.myget.org/F/wultest/api/v3/index.json --tool-path $DOTNET_TOOLS
+
+# Restore the project
+dotnet restore $REPO_ROOT/MLS-LS.sln
+
+export NUGET_PACKAGES=$DOCKER_CONTEXT_ROOT/packages
+
+# prepopulate workspaces
+
+## Console
+mkdir -p $WORKSPACES_ROOT/console
+pushd $WORKSPACES_ROOT/console
+dotnet new console
+xmlstarlet ed --inplace --insert "/Project/PropertyGroup/OutputType" --type elem -n "LangVersion" --value "7.3" console.csproj
+dotnet add package Newtonsoft.Json
+dotnet build /fl /p:ProvideCommandLineArgs=true
+popd
+
+## NodaTime
+mkdir -p $WORKSPACES_ROOT/nodatime.api
+pushd $WORKSPACES_ROOT/nodatime.api
+xmlstarlet ed --inplace --insert "/Project/PropertyGroup/OutputType" --type elem -n "LangVersion" --value "7.3" nodatime.api.csproj
+dotnet add package Newtonsoft.Json
+dotnet add package NodaTime -v 2.3.0
+dotnet add package NodaTime.Testing -v 2.3.0
+dotnet build /fl /p:ProvideCommandLineArgs=true
+popd
+
+mkdir -p $WORKSPACES_ROOT/aspnet.webapi
+pushd $WORKSPACES_ROOT/aspnet.webapi
+dotnet new webapi
+xmlstarlet ed --inplace --insert "/Project/PropertyGroup/OutputType" --type elem -n "LangVersion" --value "7.3" aspnet.webapi.csproj
+dotnet build /fl /p:ProvideCommandLineArgs=true
+dotnet publish
+popd
+
+mkdir -p $WORKSPACES_ROOT/instrumented
+pushd $WORKSPACES_ROOT/instrumented
+dotnet new console
+xmlstarlet ed --inplace --insert "/Project/PropertyGroup/OutputType" --type elem -n "LangVersion" --value "7.3" instrumented.csproj
+dotnet add package Newtonsoft.Json
+dotnet build /fl /p:ProvideCommandLineArgs=true
+popd
+
+mkdir -p $WORKSPACES_ROOT/instrumented
+pushd $WORKSPACES_ROOT/instrumented
+/workspaces/xamarin.essentials.0.8.0-preview
+dotnet new console --name console --output .
+xmlstarlet ed --inplace --insert "/Project/PropertyGroup/OutputType" --type elem -n "LangVersion" --value "7.3" console.csproj
+dotnet add package Xamarin.Essentials --version 0.8.0-preview
+dotnet build /fl /p:ProvideCommandLineArgs=true
+popd
+
+mkdir -p $WORKSPACES_ROOT/xunit
+pushd $WORKSPACES_ROOT/xunit
+dotnet new xunit --name tests --output .
+xmlstarlet ed --inplace --insert "/Project/PropertyGroup/OutputType" --type elem -n "LangVersion" --value "7.3" tests.csproj
+dotnet build /fl /p:ProvideCommandLineArgs=true
+popd
