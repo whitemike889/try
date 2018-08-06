eaf6458dd3a05d603c9c98617a9bfae96d1488e3
Nelson Wu <t-newu@microsoft.com>
2018-08-06 14:38:24 -0700

    fix

--- .cdpx/test.sh
+++ .cdpx/test.sh
@@ -6,4 +6,5 @@ REPO_ROOT=`dirname "$0"`; REPO_ROOT=`eval "cd \"$REPO_ROOT/..\" && pwd"`
 cd $REPO_ROOT
 
 dotnet test MLS.Agent.Tests/MLS.Agent.Tests.csproj --logger trx --diag:TestResults/MLS.Agent.Tests.log.txt
+dotnet test WorkspaceServer.Tests/WorkspaceServer.Tests.csproj --logger trx --diag:TestResults/WorkspaceServer.Tests.log.txt
 
