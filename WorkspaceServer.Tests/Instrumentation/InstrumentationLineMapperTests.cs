d4f565c9ab526f61b57adc48a754802cb4f5f66b
Nelson Wu <t-newu@microsoft.com>
2018-08-07 15:33:29 -0700

    address comments 2

--- WorkspaceServer.Tests/Instrumentation/InstrumentationLineMapperTests.cs
+++ WorkspaceServer.Tests/Instrumentation/InstrumentationLineMapperTests.cs
@@ -1,4 +1,6 @@
 ﻿using FluentAssertions;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using WorkspaceServer.Models.Execution;
@@ -6,25 +8,56 @@ using WorkspaceServer.Servers.Roslyn.Instrumentation;
 using WorkspaceServer.Tests.Servers.Roslyn.Instrumentation;
 using WorkspaceServer.Transformations;
 using Xunit;
+using SpanDictionary = System.Collections.Generic.IDictionary<string, System.Collections.Generic.IEnumerable<Microsoft.CodeAnalysis.Text.LinePositionSpan>>;
 
-namespace WorkspaceServer.Tests.Instrumentation
+namespace WorkspaceServer.Tests.Servers.Roslyn.Instrumentation
 {
     public class InstrumentationLineMapperTests
     {
-        private (AugmentationMap, VariableLocationMap, Microsoft.CodeAnalysis.Document, Viewport) Setup(string code)
+        private (AugmentationMap,
+            VariableLocationMap,
+            Microsoft.CodeAnalysis.Document,
+            Viewport,
+            SpanDictionary
+            ) Setup(string viewportCodeMarkup)
         {
+            MarkupTestFile.GetNamedSpans(viewportCodeMarkup, out var viewportCode, out var textSpans);
+            var code = $@"
+using System;
+using System.Linq;
+namespace RoslynRecorder
+{{
+    class Program
+    {{
+        static void Main(string[] args)
+        {{
+        int thisShouldBeIgnored = 1;
+#region test
+        {viewportCode}
+#endregion
+        }}
+    }}
+}}
+";
+            var linePositionSpans = textSpans.ToDictionary(
+                kv => kv.Key,
+                kv => kv.Value.Select(span => TextSpanToLinePositionSpanTransformer.Convert(span, SourceText.From(viewportCode))
+                    )
+                );
+
             var withLF = CodeManipulation.EnforceLF(code);
             var document = Sources.GetDocument(withLF);
             var workspace = new Workspace(files: new[] { new Workspace.File("test.cs", withLF) });
             var visitor = new InstrumentationSyntaxVisitor(document);
             var viewport = new BufferInliningTransformer().ExtractViewPorts(workspace).DefaultIfEmpty(null).First();
-            return (visitor.Augmentations, visitor.VariableLocations, document, viewport);
+
+            return (visitor.Augmentations, visitor.VariableLocations, document, viewport, linePositionSpans);
         }
 
         [Fact]
         public async Task MapLineLocationsRelativeToViewport_Does_Nothing_Without_Viewport()
         {
-            var (augmentation, locations, document, _) = Setup(Sources.simple);
+            var (augmentation, locations, document, _, _) = Setup(@"Console.WriteLine(""hello world"");");
             var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document);
 
             augmentation.Should().BeEquivalentTo(newAugmentation);
@@ -34,7 +67,80 @@ namespace WorkspaceServer.Tests.Instrumentation
         [Fact]
         public async Task MapLineLocationsRelativeToViewport_Maps_Augmentation_FilePosition_Correctly()
         {
-            var markedUpCode = @"
+            var (augmentation, locations, document, viewport, spans) = Setup(
+@"
+{|a:int a = 0;|}
+{|b:Console.WriteLine(""Entry Point"");|}
+"
+                );
+
+            var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document, viewport);
+
+            var linePositions = newAugmentation.Data.Values.Select(state => state.CurrentFilePosition.Line);
+            var expectedLinePositions = spans.Values.Select(span => span.First().Start.Line);
+            linePositions.Should().Equal(expectedLinePositions);
+        }
+
+        [Fact]
+        public async Task MapLineLocationsRelativeToViewport_Maps_Variable_Location_Correctly()
+        {
+
+            var (augmentation, locations, document, viewport, spans) = Setup(
+                @"
+{|a:int a = 0;|}
+{|b:Console.WriteLine(""Entry Point"");|}
+");
+
+            var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document, viewport);
+            var variableLocationLines = newLocations.Data.Values
+                .SelectMany(locs => locs)
+                .Select(loc => loc.StartLine);
+            var expectedLocations = spans["a"].First().Start.Line;
+
+            variableLocationLines.Should().Equal(new[] { expectedLocations });
+        }
+        
+        [Fact]
+        public async Task MapLineLocationsRelativeToViewport_Maps_Variable_At_Viewport_End_Correctly()
+        {
+            var (augmentation, locations, document, viewport, spans) = Setup(
+                @"
+{|b:Console.WriteLine(""Entry Point"");|}
+{|a:int a = 0;|}
+");
+
+            var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document, viewport);
+            var variableLocationLines = newLocations.Data.Values
+                .SelectMany(locs => locs)
+                .Select(loc => loc.StartLine);
+            var expectedLocations = spans["a"].First().Start.Line;
+
+            variableLocationLines.Should().Equal(new[] { expectedLocations });
+        }
+
+
+        [Fact]
+        public async Task MapLineLocationsRelativeToViewport_Maps_Multiple_Variables_On_Single_Line_Correctly()
+        {
+            var (augmentation, locations, document, viewport, spans) = Setup(
+                @"
+{|variables:var (a, b) = (1, 2);|}
+");
+
+            var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document, viewport);
+            var variableLocationLines = newLocations.Data.Values
+                .SelectMany(locs => locs)
+                .Select(loc => loc.StartLine)
+                .Distinct();
+            var expectedLocations = spans["variables"].First().Start.Line;
+
+            variableLocationLines.Should().Equal(new[] { expectedLocations });
+        }
+
+        [Fact]
+        public void FilterActiveViewport_Should_Return_Viewport_In_ActiveBufferId()
+        {
+            var text = CodeManipulation.EnforceLF(@"
 using System;
 namespace RoslynRecorder
 {
@@ -42,44 +148,21 @@ namespace RoslynRecorder
     {
         static void Main(string[] args)
         {
-#region test
-            {|region_start:int a = 0;|}
-            [|Console.WriteLine(""Entry Point"");|]
-#endregion
+{|regionStart:#region test|}
+            int a = 0;
+            Console.WriteLine(""Entry Point"");
+{|regionEnd:#endregion|}
         }
+#region notthis
     }
-}";
-            MarkupTestFile.GetNamedSpans(markedUpCode, out var code, out var spans);
-            var (augmentation, locations, document, viewport) = Setup(code);
-            var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document, viewport);
-
-            var linePositions = newAugmentation.Data.Values.Select(state => state.CurrentFilePosition.Line);
-            var expectedLinePositions = 
-            linePositions.Should().Equal(new[] { 0, 1 });
-        }
-
-        [Fact]
-        public async Task MapLineLocationsRelativeToViewport_Maps_Variable_Location_Correctly()
-        {
-
-            var (augmentation, locations, document, viewport) = Setup(Sources.withLocalParamsAndRegion);
-
-            var (newAugmentation, newLocations) = await InstrumentationLineMapper.MapLineLocationsRelativeToViewportAsync(augmentation, locations, document, viewport);
-            var variableLocationLines = newLocations.Data.Values
-                .SelectMany(locs => locs)
-                .Select(loc => loc.StartLine);
-
-            variableLocationLines.Should().Equal(new[] { 0 });
-        }
-
-        [Fact]
-        public void FilterActiveViewport_Should_Return_Viewport_In_ActiveBufferId()
-        {
-            var text = CodeManipulation.EnforceLF(Sources.withMultipleRegion);
-            var workspace = new Workspace(files: new[] { new Workspace.File("testFile.cs", text) });
+#endregion
+}");
+            MarkupTestFile.GetNamedSpans(text, out var code, out var spans);
+            var workspace = new Workspace(files: new[] { new Workspace.File("testFile.cs", code) });
             var viewports = new BufferInliningTransformer().ExtractViewPorts(workspace);
             var activeViewport = InstrumentationLineMapper.FilterActiveViewport(viewports, "testFile.cs@test").First();
-            activeViewport.Region.Start.Should().Be(128);
+            activeViewport.Region.Start.Should().Be(spans["regionStart"].First().End);
+            activeViewport.Region.End.Should().Be(spans["regionEnd"].First().Start);
         }
 
         [Fact]
