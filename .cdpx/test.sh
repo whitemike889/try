e298b7c163143feb474bf405fae57c8594e92d29
Jon Sequeira <jonsequeira@gmail.com>
2018-08-08 15:24:00 -0700

    set configuration to release for test runs

--- .cdpx/test.sh
+++ .cdpx/test.sh
@@ -7,7 +7,7 @@ cd $REPO_ROOT
 
 EXIT_STATUS=0
 
-dotnet test MLS.Agent.Tests/MLS.Agent.Tests.csproj --logger trx --diag:TestResults/MLS.Agent.Tests.log.txt || EXIT_STATUS=$?
-dotnet test WorkspaceServer.Tests/WorkspaceServer.Tests.csproj --logger trx --diag:TestResults/WorkspaceServer.Tests.log.txt || EXIT_STATUS=$?
+dotnet test MLS.Agent.Tests/MLS.Agent.Tests.csproj --configuration Release --logger trx --diag:TestResults/MLS.Agent.Tests.log.txt || EXIT_STATUS=$?
+dotnet test WorkspaceServer.Tests/WorkspaceServer.Tests.csproj --configuration Release --logger trx --diag:TestResults/WorkspaceServer.Tests.log.txt || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
