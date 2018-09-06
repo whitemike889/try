eeebe1ac55b1ef122f0999e47e228ee87e5d6fcc
Jon Sequeira <jonsequeira@gmail.com>
2018-09-06 11:28:37 -0700

    specify t-rex version

--- .cdpx/restore.sh
+++ .cdpx/restore.sh
@@ -15,7 +15,7 @@ apk add xmlstarlet
 apk add shadow
 
 # Add dotnet tools
-dotnet tool install -g t-rex
+dotnet tool install -g --version 1.0.62 t-rex
 
 # Restore the project
 dotnet restore $REPO_ROOT/MLS-LS.sln
