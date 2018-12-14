e6a844c7e6886caff10573a24f0693e7d3008ece
Akshita Agarwal <akagarw@microsoft.com>
2018-12-14 22:51:27 +0000

    Merged PR 1297319: Adding proper path to the local docker run
    Instead of using the C:/dev/agent path we should use the parent of the script directory

--- .cdpx/local.ps1
+++ .cdpx/local.ps1
@@ -4,18 +4,22 @@ function Get-ScriptDirectory
   Split-Path $Invocation.MyCommand.Path
 }
 
-Foreach ($file in $(Get-ChildItem -Path $(Get-ScriptDirectory)))
+Foreach ($file in $(Get-ChildItem -Path $(Get-ScriptDirectory))) 
 {
     $file = (Convert-Path $file)
     $text = [IO.File]::ReadAllText($file) -replace "`r`n", "`n"
     [IO.File]::WriteAllText($file, $text)
 }
 
+$scriptDir = (Get-ScriptDirectory)
+$agentDir = (get-item $scriptDir ).parent.FullName.ToLower()
+
 Remove-Item -Path "$(Get-ScriptDirectory)\..\docker" -Recurse -ErrorAction Ignore
 Remove-Item -Path "$(Get-ScriptDirectory)\..\.release" -Recurse -ErrorAction Ignore
 
 Copy-Item "$env:APPDATA\NuGet\NuGet.Config" "$(Get-ScriptDirectory)\..\User-NuGet.Config.tmp"
 
-docker run -it -v c:\dev\Agent:/Sources microsoft/dotnet:2.1.403-sdk-alpine /bin/sh -c '/Sources/.cdpx/local.sh'
+Write-Output "Mounting directory ${agentDir} in docker" 
+docker run -it -v ${agentDir}:/Sources microsoft/dotnet:2.1.403-sdk-alpine /bin/sh -c '/Sources/.cdpx/local.sh'
 
 t-rex.exe --path "$(Get-ScriptDirectory)/.." 
