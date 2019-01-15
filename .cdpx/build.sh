443dead5d939aefff1dfff484ec9a441b1a255fc
Ravi Chande <rchande@microsoft.com>
2019-01-15 12:48:55 -0800

    Add diag text

--- .cdpx/build.sh
+++ .cdpx/build.sh
@@ -12,6 +12,7 @@ mkdir $RELEASE_ROOT
 echo $BUILD_SOURCEVERSION > $RELEASE_ROOT/build.artifact.commit.sha
 cp $REPO_ROOT/.cdpx/release.sh $RELEASE_ROOT/release.sh
 
+echo "publish"
 dotnet publish $REPO_ROOT/MLS.Agent/MLS.Agent.csproj -c Release -o $APP_ROOT
 
 cp $REPO_ROOT/MLS.Agent/Dockerfile $DOCKER_CONTEXT_ROOT/Dockerfile
@@ -21,6 +22,7 @@ cp -r $NUGET_PACKAGES/. $DOCKER_NUGET_PACKAGES
 
 # create packages
 mkdir $PACKAGE_ROOT
+echo "package"
 dotnet pack $REPO_ROOT/MLS-LS.sln  -c Release --no-build -o $PACKAGE_ROOT /p:NoPackageAnalysis=true
 
 noLeadingZeros=$(echo $CDP_BUILD_NUMBER | sed 's/^0*//')
