c91372d60c1fe14c86f2afd7f3f186a94c6b2509
Diego <dicolomb@microsoft.com>
2019-01-18 12:21:44 +0000

    rendering with parametric session id

--- MLS.Agent/Markdown/CodeLinkBlockRenderer.cs
+++ MLS.Agent/Markdown/CodeLinkBlockRenderer.cs
@@ -29,16 +29,47 @@ namespace MLS.Agent.Markdown
                 return;
             }
 
+            var session = codeLinkBlock.GetAttributes().Properties.FirstOrDefault(p => p.Key == "data-trydotnet-session-id").Value;
+
             renderer
+                .WriteLine(@"<div class=""editor-panel"">")
                 .WriteLine(@"<pre style=""border:none"" height=""300px"" width=""800px"">")
                 .Write("<code")
                 .WriteAttributes(codeLinkBlock)
                 .WriteLine(">")
                 .WriteEscape(codeLinkBlock.CodeLines)
                 .WriteLine()
-                .WriteLine("</code></pre>")
-                .WriteLine(@"<button data-trydotnet-mode=""run"" data-trydotnet-session-id=""a"">Run</button>")
-                .WriteLine(@"<div data-trydotnet-mode=""runResult"" data-trydotnet-session-id=""a""></div>");
+                .WriteLine("</code>")
+                .WriteLine("</pre>")
+                .WriteLine(@"</div >");
+
+
+            AddRunButtonForSession(renderer, session);
+
+            AddOutputForSession(renderer, session);
+        }
+
+        private static void AddOutputForSession(HtmlRenderer renderer, string session)
+        {
+            renderer.Write(@"<div class=""output-panel"" data-trydotnet-mode=""runResult""");
+            WriteDotnetSessionAttribute(renderer, session);
+            renderer.WriteLine(@"></div>");
+        }
+
+        private static void WriteDotnetSessionAttribute(HtmlRenderer renderer, string session)
+        {
+            if (!string.IsNullOrWhiteSpace(session))
+            {
+                renderer.Write($@" data-trydotnet-session-id=""{session}""");
+            }
+        }
+
+        private static void AddRunButtonForSession(HtmlRenderer renderer, string session)
+        {
+            var buttonLabel = string.IsNullOrWhiteSpace(session) ? "Run" : $"Run {session}";
+            renderer.Write(@"<button class=""run-button"" data-trydotnet-mode=""run""");
+            WriteDotnetSessionAttribute(renderer, session);
+            renderer.WriteLine($@">{buttonLabel}</button>");
         }
     }
 }
