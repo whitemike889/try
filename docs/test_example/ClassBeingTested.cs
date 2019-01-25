7968cb963d7241a1dc8909460c67b9399965919b
Jon Sequeira <josequ@microsoft.com>
2019-01-25 00:44:27 +0000

    Merged PR 1374841: dotnet try unit testing demo
    dotnet try unit testing demo

---
+++ docs/test_example/ClassBeingTested.cs
@@ -0,0 +1,13 @@
+namespace test_example
+{
+
+    public static class ClassBeingTested
+    {
+            #region GetValue
+        public static int GetValue()
+        {
+                return 1;
+        }
+            #endregion
+    }
+}
