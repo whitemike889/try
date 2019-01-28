6339aa62ab0625bc0ac5359f6914dc3f383955f6
Diego <dicolomb@microsoft.com>
2019-01-28 23:27:28 +0000

    override version in tests

--- MLS.Agent.Tests/LocalToolStaticFilesTests.cs
+++ MLS.Agent.Tests/LocalToolStaticFilesTests.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Clockwise;
 using FluentAssertions;
 using MLS.Agent.Tools;
 using Recipes;
@@ -25,19 +26,21 @@ namespace MLS.Agent.Tests
             {
                 var dotnetTryDirectory = Package.CreateDirectory("dotnet-try-test");
 
+                var toolName = "dotnet-try";
+
                 var dotnet = new Dotnet(dotnetTryDirectory);
 
-                var result = await dotnet.Pack($"{GetAgentCsproj()} -o {dotnetTryDirectory.FullName}");
+                var result = await dotnet.Pack($"/p:Version=2.0.0 {GetAgentCsproj()} -o {dotnetTryDirectory.FullName}");
 
                 result.ThrowOnFailure("Package build failed.");
 
-                var installResult = await dotnet.ToolInstall("dotnet-try", dotnetTryDirectory.FullName, dotnetTryDirectory);
+                var installResult = await InstallDotnetTry(dotnet, toolName, dotnetTryDirectory.FullName, dotnetTryDirectory);
 
                 installResult.ThrowOnFailure("Tool installation failed.");
 
-                var toolExecutable = Path.Combine(dotnetTryDirectory.FullName, "dotnet-try");
+                var toolExecutable = Path.Combine(dotnetTryDirectory.FullName, toolName);
 
-                
+
                 toolProcess = Process.StartProcess(toolExecutable, " .", dotnetTryDirectory.FullName);
 
                 var client = new HttpClient();
@@ -56,6 +59,20 @@ namespace MLS.Agent.Tests
         private static string GetAgentCsproj([CallerFilePath] string path = null)
         {
             return Path.Combine(Path.GetDirectoryName(path), @"..", "MLS.Agent", "MLS.Agent.csproj");
+        }
+
+        public Task<CommandLineResult> InstallDotnetTry(Dotnet dotnet,
+            string packageName,
+            string toolPath,
+            DirectoryInfo addSource = null, Budget budget = null)
+        {
+            var args = $"{packageName} --tool-path {toolPath}";
+            if (addSource != null)
+            {
+                args += $" --add-source \"{addSource}\"";
+            }
+
+            return dotnet.Execute("tool install".AppendArgs(args), budget);
         }
     }
 }
