963b639aa70a574230a3fde7ae061d99de261fd9
Diego <dicolomb@microsoft.com>
2019-01-28 18:43:21 +0000

    add test

--- MLS.Agent.Tests/StaticFileTests.cs
+++ MLS.Agent.Tests/StaticFileTests.cs
@@ -13,14 +13,14 @@ namespace MLS.Agent.Tests
         public async Task Static_files_can_be_loaded_from_wwwroot_at_agent_folder()
         {
             var wwwRootPath = Path.Combine(Path.GetDirectoryName(typeof(Startup).Assembly.Location), "wwwroot");
-
+            var fileName = Guid.NewGuid().ToString("N") + ".txt";
             Directory.CreateDirectory(wwwRootPath);
             var guid = Guid.NewGuid().ToString("N");
-            File.WriteAllText(Path.Combine(wwwRootPath, "dataFile.txt"), guid);
+            File.WriteAllText(Path.Combine(wwwRootPath, fileName), guid);
 
             using (var agent = new AgentService(new StartupOptions(rootDirectory: TestAssets.SampleConsole)))
             {
-                var response = await agent.GetAsync(@"/dataFile.txt");
+                var response = await agent.GetAsync($@"/{fileName}");
 
                 response.Should().BeSuccessful();
 
@@ -31,29 +31,59 @@ namespace MLS.Agent.Tests
         }
 
         [Fact]
-        public async Task Static_files_can_be_loaded_from_wwwroot_in_root_directory_first()
+        public async Task Static_file_is_loaded_from_wwwroot_in_root_directory_first()
         {
             var wwwRootPath = Path.Combine(Path.GetDirectoryName(typeof(Startup).Assembly.Location), "wwwroot");
             var localWwwRootPath = Path.Combine(TestAssets.SampleConsole.FullName, "wwwroot");
+            var fileName = Guid.NewGuid().ToString("N") + ".txt";
 
             Directory.CreateDirectory(wwwRootPath);
             Directory.CreateDirectory(localWwwRootPath);
 
             var rootContent = Guid.NewGuid().ToString("N");
-            File.WriteAllText(Path.Combine(wwwRootPath, "dataFile.txt"), rootContent);
+            File.WriteAllText(Path.Combine(wwwRootPath, fileName), rootContent);
 
             var localContent = Guid.NewGuid().ToString("N");
-            File.WriteAllText(Path.Combine(localWwwRootPath, "dataFile.txt"), localContent);
+            File.WriteAllText(Path.Combine(localWwwRootPath, fileName), localContent);
 
             using (var agent = new AgentService(new StartupOptions(rootDirectory: TestAssets.SampleConsole)))
             {
-                var response = await agent.GetAsync(@"/dataFile.txt");
+                var response = await agent.GetAsync($@"/{fileName}");
 
                 response.Should().BeSuccessful();
 
                 var content = await response.Content.ReadAsStringAsync();
 
                 content.Should().Be(localContent);
+            }
+        }
+
+        [Fact]
+        public async Task Static_file_is_loaded_from_wwwroot_in_tool_directory_if_not_found_in_root_directory()
+        {
+            var wwwRootPath = Path.Combine(Path.GetDirectoryName(typeof(Startup).Assembly.Location), "wwwroot");
+            var localWwwRootPath = Path.Combine(TestAssets.SampleConsole.FullName, "wwwroot");
+            var localFileName = Guid.NewGuid().ToString("N") + ".txt";
+            var rootFileName = Guid.NewGuid().ToString("N") + ".txt";
+
+            Directory.CreateDirectory(wwwRootPath);
+            Directory.CreateDirectory(localWwwRootPath);
+
+            var rootContent = Guid.NewGuid().ToString("N");
+            File.WriteAllText(Path.Combine(wwwRootPath, rootFileName), rootContent);
+
+            var localContent = Guid.NewGuid().ToString("N");
+            File.WriteAllText(Path.Combine(localWwwRootPath, localFileName), localContent);
+
+            using (var agent = new AgentService(new StartupOptions(rootDirectory: TestAssets.SampleConsole)))
+            {
+                var response = await agent.GetAsync($@"/{rootFileName}");
+
+                response.Should().BeSuccessful();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                content.Should().Be(rootContent);
             }
         }
     }
