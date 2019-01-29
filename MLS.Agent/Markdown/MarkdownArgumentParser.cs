adcb75e7d34a24f4ec704b393968bb1da3e7562b
Ravi Chande <rchande@microsoft.com>
2019-01-29 10:45:24 -0800

    Revert "cleanup"
    This reverts commit 8df3a9564dd28bac460baca97346fd39d6ccfe88.

--- MLS.Agent/Markdown/MarkdownArgumentParser.cs
+++ MLS.Agent/Markdown/MarkdownArgumentParser.cs
@@ -50,7 +50,12 @@ namespace MLS.Agent.Markdown
 
                                         if (RelativeFilePath.TryParse(filename, out var relativeFilePath))
                                         {
-                                            return ArgumentResult.Success(relativeFilePath);
+                                            if (directoryAccessor.FileExists(relativeFilePath))
+                                            {
+                                                return ArgumentResult.Success(relativeFilePath);
+                                            }
+
+                                            return ArgumentResult.Failure($"File not found: {relativeFilePath.Value}");
                                         }
 
                                         return ArgumentResult.Failure($"Error parsing the filename: {filename}");
