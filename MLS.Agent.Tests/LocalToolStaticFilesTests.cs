86d9a7173e9d721b7a87cefce91cbcd09bd6b349
Diego <dicolomb@microsoft.com>
2019-01-29 10:32:48 +0000

    fixed test

--- MLS.Agent.Tests/LocalToolStaticFilesTests.cs
+++ MLS.Agent.Tests/LocalToolStaticFilesTests.cs
@@ -6,47 +6,15 @@ using System.Threading.Tasks;
 using Assent;
 using Clockwise;
 using MLS.Agent.Tools;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Pocket;
 using WorkspaceServer;
 using WorkspaceServer.Packaging;
-using WorkspaceServer.Tests;
 using Xunit;
 using Xunit.Abstractions;
 using Configuration = Assent.Configuration;
 
 namespace MLS.Agent.Tests
 {
-    internal static class DirectoryInfoExtensions
-    {
-        public static string ToJsonStructure(this DirectoryInfo source)
-        {
-            var report =  new JObject
-            {
-                {"name", source.Name }
-            };
-
-            var rootPath = source.FullName;
-
-            var content = new JArray( source
-                .GetFiles("*", SearchOption.AllDirectories)
-                .OrderBy(f => f.Directory?.FullName.Split(Environment.NewLine).Length ?? 0)
-                .ThenBy(f => f.FullName)
-                .Select(f => f.ToJsonStructure(rootPath)));
-
-            report["content"] = content;
-            return report.ToString().FormatJson();
-        }
-
-        private static JToken ToJsonStructure(this FileSystemInfo source, string root)
-        {
-            return new JObject
-            {
-                {"name", source.FullName.Replace(root, string.Empty).Replace("\\", "/") }
-            };
-        }
-    }
     public class LocalToolStaticFilesTests : IDisposable
     {
         private readonly Configuration _configuration;
@@ -61,7 +29,7 @@ namespace MLS.Agent.Tests
                 .UsingExtension("json");
 
 #if !DEBUG
-            configuration = configuration.SetInteractive(false);
+            _configuration = configuration.SetInteractive(false);
 #endif
         }
         [Fact]
