fd6eab3c4f4e0c11dc72cef4d2961cf8ab43e1b5
Ravi Chande <rchande@microsoft.com>
2019-02-21 08:19:24 -0800

    Copy blazor stuff into agent

---
+++ WasmCodeRunner/CompileResult.cs
@@ -0,0 +1,9 @@
+﻿namespace MLS.WasmCodeRunner
+{
+    public class CompileResult
+    {
+        public bool succeeded { get; set; }
+        public string base64assembly { get; set; }
+        public SerializableDiagnostic[] diagnostics { get; set; }
+    }
+}
---
+++ WasmCodeRunner/RunResponse.cs
@@ -0,0 +1,25 @@
+﻿using Recipes;
+
+namespace MLS.WasmCodeRunner
+{
+    public class RunResponse
+    {
+
+        public RunResponse(bool succeeded, string exception, string[] output, SerializableDiagnostic[] diagnostics, string runnerException)
+        {
+            this.exception = exception;
+            this.output = output;
+            this.succeeded = succeeded;
+            this.diagnostics = diagnostics;
+            this.runnerException = runnerException;
+        }
+
+        public string exception { get;}
+        public string[] output { get; }
+        public bool succeeded { get; }
+        public SerializableDiagnostic[] diagnostics { get; }
+        public string runnerException { get; }
+        public string codeRunnerVersion => VersionSensor.Version().AssemblyInformationalVersion;
+
+    }
+}
