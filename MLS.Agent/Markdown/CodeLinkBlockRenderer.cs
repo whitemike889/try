977c34a160c019b1b7fcfe306c704421a615d380
Diego <dicolomb@microsoft.com>
2019-03-19 22:06:37 +0000

    use material svgs

--- MLS.Agent/Markdown/CodeLinkBlockRenderer.cs
+++ MLS.Agent/Markdown/CodeLinkBlockRenderer.cs
@@ -59,7 +59,9 @@ namespace MLS.Agent.Markdown
 
             if (InlineControls)
             {
-                const string playSvg = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 9 10\"><path fill=\"white\" d=\"M1,0 1,10, 9,5z\" /></svg>";
+                //const string playSvg = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 9 10\"><path fill=\"white\" d=\"M1,0 1,10, 9,5z\" /></svg>";
+
+                const string playSvg = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M8,5.14V19.14L19,12.14L8,5.14Z\" /></svg>";
 
                 renderer
                     .WriteLine($@"<button class=""run"" data-trydotnet-mode=""run"" data-trydotnet-session-id=""{codeLinkBlock.Session}"" data-trydotnet-run-args=""{codeLinkBlock.RunArgs.HtmlAttributeEncode()}"">{playSvg}</button>");
