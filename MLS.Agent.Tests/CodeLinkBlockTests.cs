177dd606a65c1bd35bf2f35139453637e5a1760f
Diego <dicolomb@microsoft.com>
2019-04-01 15:13:00 +0100

    refactor test code

--- MLS.Agent.Tests/CodeLinkBlockTests.cs
+++ MLS.Agent.Tests/CodeLinkBlockTests.cs
@@ -3,7 +3,6 @@ using System;
 using Xunit;
 using FluentAssertions;
 using System.Threading.Tasks;
-using System.IO;
 using WorkspaceServer;
 using WorkspaceServer.Tests;
 
@@ -12,19 +11,18 @@ namespace MLS.Agent.Tests
     public class CodeLinkBlockTests
     {
         private readonly CodeLinkBlockParser _parser;
+        private readonly InMemoryDirectoryAccessor _directoryAccessor;
 
         public CodeLinkBlockTests()
         {
             var rootDirectory = Create.EmptyWorkspace(isRebuildablePackage: true).Directory;
-            var directoryAccessor = new InMemoryDirectoryAccessor(rootDirectory);
+            _directoryAccessor = new InMemoryDirectoryAccessor(rootDirectory);
             var registry = PackageRegistry.CreateForTryMode(rootDirectory);
-            _parser = new CodeLinkBlockParser(directoryAccessor, registry);
+            _parser = new CodeLinkBlockParser(_directoryAccessor, registry);
         }
         [Fact]
         public void It_requires_options_to_initialize()
         {
-          
-
             var block = new CodeLinkBlock(_parser, 0);
             block.Invoking(b => b.InitializeAsync().Wait())
                 .Should().Throw<InvalidOperationException>("Attempted to initialize block before adding options");
@@ -43,18 +41,14 @@ namespace MLS.Agent.Tests
         [Fact]
         public async Task It_uses_the_provided_directory_accessor()
         {
-            DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
-            var accessor = new InMemoryDirectoryAccessor(di, di)
-            {
-                ("Program.cs", "foo")
-            };
+            _directoryAccessor.Add(("Program.cs", "foo"));
 
             var options = new CodeLinkBlockOptions(
                 sourceFile: new RelativeFilePath("Program.cs"),
                 package: "the-package").WithIsProjectImplicit(true);
 
             var block = new CodeLinkBlock(_parser, 0);
-            block.AddOptions(options, () => Task.FromResult<IDirectoryAccessor>(accessor));
+            block.AddOptions(options, () => Task.FromResult<IDirectoryAccessor>(_directoryAccessor));
             await block.InitializeAsync();
             block.SourceCode.Should().Be("foo");
         }
