eb01174a01fa918bee639f288a9d2f1f8a5576f7
Diego <dicolomb@microsoft.com>
2019-04-04 09:43:58 +0100

    pr feedback

--- WorkspaceServer/Packaging/FileInfoExtensions.cs
+++ WorkspaceServer/Packaging/FileInfoExtensions.cs
@@ -7,30 +7,52 @@ namespace WorkspaceServer.Packaging
 {
     internal static class FileInfoExtensions
     {
-        public static  string GetTargetFramework(this FileInfo project)
+        public static string GetTargetFramework(this FileInfo project)
         {
-            if (project.Exists)
+            if (project == null)
             {
-                var dom = XElement.Parse(File.ReadAllText(project.FullName));
-                var targetFramework = dom.XPathSelectElement("//TargetFramework");
-                return targetFramework?.Value ?? string.Empty;
+                throw new ArgumentNullException(nameof(project));
             }
-            return string.Empty;
+
+            if (!project.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
+            var dom = XElement.Parse(File.ReadAllText(project.FullName));
+            var targetFramework = dom.XPathSelectElement("//TargetFramework");
+            return targetFramework?.Value ?? string.Empty;
         }
 
         public static string GetLanguageVersion(this FileInfo project)
         {
-            if (project.Exists)
+            if (project == null)
             {
-                var dom = XElement.Parse(File.ReadAllText(project.FullName));
-                var languageVersion = dom.XPathSelectElement("//LangVersion");
-                return languageVersion?.Value ?? "7.3";
+                throw new ArgumentNullException(nameof(project));
             }
-            return string.Empty;
+
+            if (!project.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
+            var dom = XElement.Parse(File.ReadAllText(project.FullName));
+            var languageVersion = dom.XPathSelectElement("//LangVersion");
+            return languageVersion?.Value ?? "7.3";
         }
 
         public static void SetLanguageVersion(this FileInfo project, string version)
         {
+            if (project == null)
+            {
+                throw new ArgumentNullException(nameof(project));
+            }
+
+            if (!project.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
             var dom = XElement.Parse(File.ReadAllText(project.FullName));
             var langElement = dom.XPathSelectElement("//LangVersion");
 
@@ -49,26 +71,22 @@ namespace WorkspaceServer.Packaging
 
         public static void TrySetLanguageVersion(this FileInfo project, string version)
         {
+            if (project == null)
+            {
+                throw new ArgumentNullException(nameof(project));
+            }
+
+            if (!project.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
             var supported = CSharpLanguageSelector.GetCSharpLanguageVersion(project.GetTargetFramework());
 
-            var canSet = StringComparer.OrdinalIgnoreCase.Equals( supported ,version);
+            var canSet = StringComparer.OrdinalIgnoreCase.Equals(supported, version);
             if (canSet)
             {
-
-                var dom = XElement.Parse(File.ReadAllText(project.FullName));
-                var langElement = dom.XPathSelectElement("//LangVersion");
-
-                if (langElement != null)
-                {
-                    langElement.Value = version;
-                }
-                else
-                {
-                    var propertyGroup = dom.XPathSelectElement("//PropertyGroup");
-                    propertyGroup?.Add(new XElement("LangVersion", version));
-                }
-
-                File.WriteAllText(project.FullName, dom.ToString());
+                project.SetLanguageVersion(version);
             }
         }
     }
