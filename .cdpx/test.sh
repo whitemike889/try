7c29ce1a561c40e97b3b7ab7e70d5d321850217a
Diego <dicolomb@microsoft.com>
2019-04-09 12:41:48 +0100

    change test run order

--- .cdpx/test.sh
+++ .cdpx/test.sh
@@ -7,12 +7,11 @@ cd $REPO_ROOT
 
 EXIT_STATUS=0
 
-dotnet test MLS.Agent.Tests/MLS.Agent.Tests.csproj --configuration Release --logger trx --diag:TestResults/MLS.Agent.Tests.log.txt || EXIT_STATUS=$?
-dotnet test WorkspaceServer.Tests/WorkspaceServer.Tests.csproj --configuration Release --logger trx --diag:TestResults/WorkspaceServer.Tests.log.txt || EXIT_STATUS=$?
-
-dotnet test Microsoft.DotNet.Try.Jupyter.Tests/Microsoft.DotNet.Try.Jupyter.Tests.csproj --configuration Release --logger trx --diag:TestResults/Microsoft.DotNet.Try.Jupyter.Tests.log.txt || EXIT_STATUS=$?
-dotnet test Microsoft.DotNet.Try.Markdown.Tests/Microsoft.DotNet.Try.Markdown.Tests.csproj --configuration Release --logger trx --diag:TestResults/Microsoft.DotNet.Try.Markdown.Tests.log.txt || EXIT_STATUS=$?
 dotnet test Microsoft.DotNet.Try.Project.Tests/Microsoft.DotNet.Try.Project.Tests.csproj --configuration Release --logger trx --diag:TestResults/Microsoft.DotNet.Try.Project.Tests.log.txt || EXIT_STATUS=$?
 dotnet test Microsoft.DotNet.Try.Protocol.Tests/Microsoft.DotNet.Try.Protocol.Tests.csproj --configuration Release --logger trx --diag:TestResults/Microsoft.DotNet.Try.Protocol.Tests.log.txt || EXIT_STATUS=$?
+dotnet test WorkspaceServer.Tests/WorkspaceServer.Tests.csproj --configuration Release --logger trx --diag:TestResults/WorkspaceServer.Tests.log.txt || EXIT_STATUS=$?
+dotnet test Microsoft.DotNet.Try.Jupyter.Tests/Microsoft.DotNet.Try.Jupyter.Tests.csproj --configuration Release --logger trx --diag:TestResults/Microsoft.DotNet.Try.Jupyter.Tests.log.txt || EXIT_STATUS=$?
+dotnet test Microsoft.DotNet.Try.Markdown.Tests/Microsoft.DotNet.Try.Markdown.Tests.csproj --configuration Release --logger trx --diag:TestResults/Microsoft.DotNet.Try.Markdown.Tests.log.txt || EXIT_STATUS=$?
+dotnet test MLS.Agent.Tests/MLS.Agent.Tests.csproj --configuration Release --logger trx --diag:TestResults/MLS.Agent.Tests.log.txt || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
